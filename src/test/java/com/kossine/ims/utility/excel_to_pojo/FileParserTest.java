3c043f2d195c7fb0d5b3fea0abc325a991d6aa61
jigarWala <jigar.wala@somaiya.edu>
2017-12-14 10:38:04 +0530

    tested utility/excel_to_pojo/FileParser with injected properties

--- src/test/java/com/kossine/ims/utility/excel_to_pojo/FileParserTest.java
+++ src/test/java/com/kossine/ims/utility/excel_to_pojo/FileParserTest.java
@@ -3,21 +3,31 @@ package com.kossine.ims.utility.excel_to_pojo;
 import java.io.File;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.kossine.ims.models.Inventory;
 import com.kossine.ims.models.Laptop;
-import com.kossine.ims.utility.excel_to_pojo.FileParser;
 
+@TestPropertySource(locations = "classpath:application.properties")
+@RunWith(SpringRunner.class)
 public class FileParserTest {
+
+	@Value("${json_format_file}")
+	String formatfile;
+	@Value("${excel_file}")
+	String excelfile;
 
 	@Test
 	public void test() throws Exception {
-		File formatFile=new File("/home/jigar/workspace/ims/src/main/java/com/kossine/ims/utility/excel_to_pojo/format.json");
-		File excel=new File("/home/jigar/workspace/projectdata1.xlsx");
-		FileParser fp=new FileParser(excel,formatFile);
+		File formatFile = new File(formatfile);
+		File excel = new File(excelfile);
+		FileParser fp = new FileParser(excel, formatFile);
 
-		for(Inventory e : fp.parseFile().get(Laptop.class))
-			System.out.println(((Laptop)e).getDop());
+		for (Inventory e : fp.parseFile().get(Laptop.class))
+			System.out.println(((Laptop) e).getDop());
 	}
 
 }
