9ce89101c3b48a78510e6d230a491e019ad0c24d
jigarWala <jigar.wala@somaiya.edu>
2018-01-05 18:35:50 +0530

    :fire:

--- src/main/java/com/kossine/ims/ImsApplication.java
+++ src/main/java/com/kossine/ims/ImsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ImsApplication {
 
 	public static void main(String[] args) {
